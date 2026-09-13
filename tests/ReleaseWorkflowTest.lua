9a15b175e6289aa8b0163ae7d32c37c4d7c7c0ef
Vadim Tofan <vad.tofan@gmail.com>
2026-09-13 19:59:26 +0200

    chore(release): bump version to 0.1.11

--- tests/ReleaseWorkflowTest.lua
+++ tests/ReleaseWorkflowTest.lua
@@ -56,7 +56,7 @@ TestRunner.describe("CurseForge release workflow", function()
             true
         ) ~= nil
         local hasReleaseVersion = toc:find(
-            "## Version: 0.1.10",
+            "## Version: 0.1.11",
             1,
             true
         ) ~= nil
